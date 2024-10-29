d529837511113b300b2596ea92b6ab726f459c84
RaulMetriTorres <raul.metri@gmail.com>
2024-10-29 00:44:16 -0600

    ejercicio 5 completado

--- exercise.js
+++ exercise.js
@@ -11,7 +11,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+
+        // Si el arreglo está vacío, devolvemos un arreglo vacío
+        if (arr.length === 0) return [];
+
+        // Usamos reduce para "aplanar" el arreglo
+        return arr.reduce((acc, val) => {
+            // Verificamos si el elemento es un arreglo
+            // Si es un arreglo, llamamos a flatten recursivamente
+            // Si no, lo agregamos directamente al acumulador
+            return acc.concat(Array.isArray(val) ? flatten(val) : val);
+        }, []);
+
     /* Only make changes below this comment */
 }
 
