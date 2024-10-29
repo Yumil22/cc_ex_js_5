78a7f858f09f43f6d6ba528b1cd291e6283a4814
rodrigoARP <rodrigo.adrian.rodriguezpuerto@gmail.com>
2024-10-28 23:10:50 -0600

    Ejercicio 5 listo

--- exercise.js
+++ exercise.js
@@ -11,7 +11,19 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const flatten = (arr) => {
     /* Only make changes below this comment */
-    
+    let flattArray = [];
+
+    arr.forEach(element => {
+        if(Array.isArray(element)){
+            flattArray = flattArray.concat(flatten(element));
+        }
+
+        else{
+            flattArray.push(element);
+        }
+    });
+
+    return flattArray;
     /* Only make changes below this comment */
 }
 
